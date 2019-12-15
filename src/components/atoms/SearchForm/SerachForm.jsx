2abf83ab796070a7743891748afe3e393aaf8cef
Nadia Abe <abe.nami0510@gmail.com>
2019-12-15 15:53:38 -0300

    feat(search-movies)

---
+++ src/components/atoms/SearchForm/SerachForm.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class SearchForm extends Component {
+  state = {
+    name: ""
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+
+    this.setState({ [name]: value }, () => {
+      const { name } = this.state;
+      const { search } = this.props;
+
+      search(name);
+    });
+  };
+
+  render() {
+    return (
+      <input
+        type="text"
+        name="name"
+        placeholder="Pesquise um filme"
+        onChange={e => this.handleChange(e)}
+      />
+    );
+  }
+}
+
+export default SearchForm;
